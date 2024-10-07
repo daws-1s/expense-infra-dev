541b471d6884360feff9b9b8b091842fb5738ff5
divya kamatham <divyakamatham12@gmai.com>
2024-10-07 22:46:14 +0800

    Add files

--- 50-vpn/data.tf
+++ 50-vpn/data.tf
@@ -1,6 +1,5 @@
-data "aws_ssm_parameter" "bastion_sg_id" {
-  #/expense/dev/bastion_sg_id
-  name = "/${var.project_name}/${var.environment}/bastion_sg_id"
+data "aws_ssm_parameter" "vpn_sg_id" {
+  name = "/${var.project_name}/${var.environment}/vpn_sg_id"
 }
 
 data "aws_ssm_parameter" "public_subnet_ids" {
@@ -11,7 +10,7 @@ data "aws_ssm_parameter" "public_subnet_ids" {
 data "aws_ami" "joindevops" {
 
 	most_recent      = true
-	owners = ["973714476881"]
+	owners = ["679593333241"]
 	
 	filter {
 		name   = "name"
