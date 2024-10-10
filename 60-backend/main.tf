ce1360ad4514b442435767b438e27227eee728cc
divya kamatham <divyakamatham12@gmai.com>
2024-10-10 10:02:14 +0800

    Add files

--- 60-backend/main.tf
+++ 60-backend/main.tf
@@ -1,7 +1,7 @@
 module "backend" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  ami = data.aws_ami.joindevops.id
-  name = local.resource_name
+  source = "terraform-aws-modules/ec2-instance/aws"
+  ami    = data.aws_ami.joindevops.id
+  name   = local.resource_name
 
   instance_type          = "t3.micro"
   vpc_security_group_ids = [local.backend_sg_id]
@@ -11,7 +11,7 @@ module "backend" {
     var.common_tags,
     var.backend_tags,
     {
-        Name = local.resource_name
+      Name = local.resource_name
     }
   )
 }
@@ -25,13 +25,13 @@ resource "null_resource" "backend" {
   # Bootstrap script can run on any instance of the cluster
   # So we just choose the first in this case
   connection {
-    host = module.backend.private_ip
-    type = "ssh"
-    user = "ec2-user"
+    host     = module.backend.private_ip
+    type     = "ssh"
+    user     = "ec2-user"
     password = "DevOps321"
   }
 
-   provisioner "file" {
+  provisioner "file" {
     source      = "${var.backend_tags.Component}.sh"
     destination = "/tmp/backend.sh"
   }
@@ -49,13 +49,13 @@ resource "null_resource" "backend" {
 resource "aws_ec2_instance_state" "backend" {
   instance_id = module.backend.id
   state       = "stopped"
-  depends_on = [null_resource.backend]
+  depends_on  = [null_resource.backend]
 }
 
 resource "aws_ami_from_instance" "backend" {
   name               = local.resource_name
   source_instance_id = module.backend.id
-  depends_on = [aws_ec2_instance_state.backend]
+  depends_on         = [aws_ec2_instance_state.backend]
 }
 
 resource "null_resource" "backend_delete" {
@@ -71,20 +71,39 @@ resource "null_resource" "backend_delete" {
   depends_on = [aws_ami_from_instance.backend]
 }
 
-# resource "aws_lb_target_group" "backend" {
-#   name     = local.resource_name
-#   port     = 8080
-#   protocol = "HTTP"
-#   vpc_id   = local.vpc_id
+resource "aws_lb_target_group" "backend" {
+  name     = local.resource_name
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
 
-#   health_check {
-#     healthy_threshold = 2
-#     unhealthy_threshold = 2
-#     interval = 5
-#     matcher = "200-299"
-#     path = "/health"
-#     port = 8080
-#     protocol = "HTTP"
-#     timeout = 4
-#   }
-# }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 5
+    matcher             = "200-299"
+    path                = "/health"
+    port                = 8080
+    protocol            = "HTTP"
+    timeout             = 4
+  }
+}
+
+resource "aws_launch_template" "backend" {
+
+  name                                 = local.resource_name
+  image_id                             = aws_ami_from_instance.backend.id
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type                        = "t3.micro"
+
+  update_default_version = true
+  vpc_security_group_ids = [local.backend_sg_id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = local.resource_name
+    }
+  }
+}
