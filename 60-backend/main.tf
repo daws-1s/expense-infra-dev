b7b6360d8a7befff295d4f9c04cbc2fa12e7f9b4
divya kamatham <divyakamatham12@gmai.com>
2024-10-10 14:03:38 +0800

    Add files

--- 60-backend/main.tf
+++ 60-backend/main.tf
@@ -107,3 +107,58 @@ resource "aws_launch_template" "backend" {
     }
   }
 }
+
+resource "aws_autoscaling_group" "backend" {
+  name                      = local.resource_name
+  max_size                  = 10
+  min_size                  = 2
+  health_check_grace_period = 60
+  health_check_type         = "ELB"
+  desired_capacity          = 2 # starting of the auto scaling group
+  target_group_arns = [aws_lb_target_group.backend.arn]
+  #force_delete              = true
+  launch_template {
+    id      = aws_launch_template.backend.id
+    version = "$Latest"
+  }
+  
+  vpc_zone_identifier       = [local.private_subnet_id]
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["launch_template"]
+  }
+
+  tag {
+    key                 = "Name"
+    value               = local.resource_name
+    propagate_at_launch = true
+  }
+
+  # If instances are not healthy with in 15min, autoscaling will delete that instance
+  timeouts {
+    delete = "15m"
+  }
+
+  tag {
+    key                 = "Project"
+    value               = "Expense"
+    propagate_at_launch = false
+  }
+}
+
+resource "aws_autoscaling_policy" "example" {
+  name = local.resource_name
+  policy_type            = "TargetTrackingScaling"
+  autoscaling_group_name  = aws_autoscaling_group.backend.name
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 70.0
+  }
+}
