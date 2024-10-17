74bd2f3f528e4e9614e016b0b4bde4a0ac99d4af
divya kamatham <divyakamatham12@gmai.com>
2024-10-17 15:19:12 +0800

    Add files

--- 50-app-alb/main.tf
+++ 50-app-alb/main.tf
@@ -1,12 +1,12 @@
 module "app_alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  internal = true
-  name    = "${local.resource_name}-app-alb" #expense-dev-app-alb
-  vpc_id  = local.vpc_id
-  subnets = local.private_subnet_ids
-  security_groups = [data.aws_ssm_parameter.app_alb_sg_id.value]
-  create_security_group = false
+  internal                   = true
+  name                       = "${local.resource_name}-app-alb" #expense-dev-app-alb
+  vpc_id                     = local.vpc_id
+  subnets                    = local.private_subnet_ids
+  security_groups            = [data.aws_ssm_parameter.app_alb_sg_id.value]
+  create_security_group      = false
   enable_deletion_protection = false
   tags = merge(
     var.common_tags,
@@ -29,16 +29,15 @@ resource "aws_lb_listener" "http" {
     }
   }
 }
-
 module "records" {
-  source  = "terraform-aws-modules/route53/aws//modules/records"
+  source = "terraform-aws-modules/route53/aws//modules/records"
 
   zone_name = var.zone_name #daws1s.online
   records = [
     {
-      name    = "*.app-${var.environment}" # *.app-dev
-      type    = "A"
-      alias   = {
+      name = "*.app-${var.environment}" # *.app-dev
+      type = "A"
+      alias = {
         name    = module.app_alb.dns_name
         zone_id = module.app_alb.zone_id # This belongs ALB internal hosted zone, not ours
       }
